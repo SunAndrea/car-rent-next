60ac2b5e9ee3748cf75830f42d31710422040308
Andrii Shtohryn <andriishtohryn@gmail.com>
2023-09-20 13:34:34 +0300

    add footer and navbar

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,6 +17,7 @@ const Hero = () => {
           process.
         </p>
         <CustomButton
+          btnType="button"
           title="Explore Cars"
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
@@ -25,8 +26,8 @@ const Hero = () => {
       <div className="hero__image-container">
         <div className="hero__image">
           <Image src="/hero.png" alt="hero" fill className="object-contain" />
-          <div className="hero__image-overlay"></div>
         </div>
+        <div className="hero__image-overlay"></div>
       </div>
     </div>
   );
